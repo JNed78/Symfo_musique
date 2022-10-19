5f9392aa265385a91d97bfc095f445ea9516b2f2
JNed78 <jng0549@gmail.com>
2022-10-19 12:21:23 +0200

    19/10 v1

--- src/Controller/AlbumController.php
+++ src/Controller/AlbumController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use App\Entity\Album;
 use App\Repository\AlbumRepository;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -14,9 +16,13 @@ class AlbumController extends AbstractController
      * @Route("/albums", name="albums", methods={"GET"})
      */
 
-    public function listealbums(AlbumRepository $repo): Response
+    public function listealbums(AlbumRepository $repo, PaginatorInterface $paginator, Request $request): Response
     {
-        $albums=$repo->listeAlbumsComplete();
+        $albums = $pagination = $paginator->paginate(
+            $repo->listeAlbumsComplete(), /* query NOT result */
+            $request->query->getInt('page', 1), /*page number*/
+            9 /*limit per page*/
+        );
         return $this->render('Album/listeAlbums.html.twig', [
             'lesAlbums'=> $albums
         ]);
