bfd815f42aaf123fe87651c9f0406c82ca0d1c30
JNed78 <jng0549@gmail.com>
2022-11-05 18:13:45 +0100

    gestions artistes

--- migrations/Version20221104225255.php
+++ migrations/Version20221104225255.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20221019103530 extends AbstractMigration
+final class Version20221011061736 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -24,11 +24,12 @@ final class Version20221019103530 extends AbstractMigration
         $this->addSql('ALTER TABLE morceau DROP FOREIGN KEY FK_36BB72081137ABCF');
         $this->addSql('ALTER TABLE style_album DROP FOREIGN KEY FK_F34D20B8BACD6074');
         $this->addSql('ALTER TABLE style_album DROP FOREIGN KEY FK_F34D20B81137ABCF');
-        
+
         $this->addSql('ALTER TABLE album CHANGE id id INT AUTO_INCREMENT NOT NULL');
         $this->addSql('ALTER TABLE artiste CHANGE id id INT AUTO_INCREMENT NOT NULL');
         $this->addSql('ALTER TABLE morceau CHANGE id id INT AUTO_INCREMENT NOT NULL');
-
+        $this->addSql('ALTER TABLE style CHANGE id id INT AUTO_INCREMENT NOT NULL');
+        
         $this->addSql('ALTER TABLE album ADD CONSTRAINT FK_39986E4321D25844 FOREIGN KEY (artiste_id) REFERENCES artiste (id)');
         $this->addSql('ALTER TABLE morceau ADD CONSTRAINT FK_36BB72081137ABCF FOREIGN KEY (album_id) REFERENCES album (id)');
         $this->addSql('ALTER TABLE style_album ADD CONSTRAINT FK_F34D20B8BACD6074 FOREIGN KEY (style_id) REFERENCES style (id) ON DELETE CASCADE');
@@ -41,5 +42,6 @@ final class Version20221019103530 extends AbstractMigration
         $this->addSql('ALTER TABLE album CHANGE id id INT NOT NULL');
         $this->addSql('ALTER TABLE artiste CHANGE id id INT NOT NULL');
         $this->addSql('ALTER TABLE morceau CHANGE id id INT NOT NULL');
+        $this->addSql('ALTER TABLE style CHANGE id id INT NOT NULL');
     }
 }
