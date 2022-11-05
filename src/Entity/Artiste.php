e9ae142edb227b4143d746436d4d6a13235ce34d
JNed78 <jng0549@gmail.com>
2022-11-05 21:24:12 +0100

    Validations

--- src/Entity/Artiste.php
+++ src/Entity/Artiste.php
@@ -5,10 +5,16 @@ namespace App\Entity;
 use App\Repository\ArtisteRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity(repositoryClass=ArtisteRepository::class)
+ * @UniqueEntity(
+ *     fields={"nom"},
+ *     message="Le nom de l'artiste est déjà utilisé dans la base"
+ * )
  */
 class Artiste
 {
@@ -21,11 +27,18 @@ class Artiste
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank(message="Le nom est obligatoire")
      */
     private $nom;
 
     /**
      * @ORM\Column(type="text")
+     *  @Assert\Length(
+     *      min = 10,
+     *      max = 50,
+     *      minMessage = "La description doit comporter au minimum {{ limit }}  caractères",
+     *      maxMessage = "La description doit comporter au maximum {{ limit }}  characters"
+     * )
      */
     private $description;
 
